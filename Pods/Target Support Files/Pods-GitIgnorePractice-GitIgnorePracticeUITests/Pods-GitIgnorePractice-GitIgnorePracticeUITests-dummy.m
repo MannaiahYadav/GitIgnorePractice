7494c296b3794c37a17b495151f25f06462b5b07
Mannaiah ubbarapu <mannaiah.jl@gmail.com>
2023-12-29 09:32:26 +0530

    new local changes

---
+++ Pods/Target Support Files/Pods-GitIgnorePractice-GitIgnorePracticeUITests/Pods-GitIgnorePractice-GitIgnorePracticeUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GitIgnorePractice_GitIgnorePracticeUITests : NSObject
+@end
+@implementation PodsDummy_Pods_GitIgnorePractice_GitIgnorePracticeUITests
+@end
---
+++ Pods/Target Support Files/Pods-GitIgnorePracticeTests/Pods-GitIgnorePracticeTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GitIgnorePracticeTests : NSObject
+@end
+@implementation PodsDummy_Pods_GitIgnorePracticeTests
+@end
